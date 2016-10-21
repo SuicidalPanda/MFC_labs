abca2a094155aa6b2a0ad856f51cbc1377c52172
Pavel Pankov <suicidalpanda@mail.ru>
2016-10-21 20:10:00 +0400

    Lesson 4 files and added lesson 4 .gitignore

---
+++ Labs/Lesson 4/Lesson 4Dlg.h
@@ -0,0 +1,32 @@
+
+// Lesson 4Dlg.h : header file
+//
+
+#pragma once
+
+
+// CLesson4Dlg dialog
+class CLesson4Dlg : public CDialogEx
+{
+// Construction
+public:
+	CLesson4Dlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_LESSON4_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
